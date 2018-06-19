2d6fbd2108cc1b1f75b868a1e7c933086ce8c1bd
fanwei <fanwei207@qq.com>
2018-06-19 10:17:00 +0800

    测试文件2

---
+++ NewsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCbilibili.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Show(string ID)
+        {
+            
+            ViewBag.ID = ID;
+            return View();
+        }
+
+
+
+    }
+}
